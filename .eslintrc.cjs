a8dacb848c612309a7e7ba8715b71e4206c2eaf5
Yudis <yudis10@users.noreply.github.com>
2023-11-30 10:59:48 +0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:tailwindcss/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "tailwindcss"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "tailwindcss/no-custom-classname": "off",
+  },
+  settings: {
+    tailwindcss: {
+      callees: ["cn"],
+      config: "tailwind.config.js",
+    },
+  },
+}
---
+++ src/main.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import MainLayout from "@/layouts/MainLayout"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import "@/styles/globals.css"
+
+import About from "@/pages/About"
+import Error from "@/pages/Error"
+import Home, { homeAction, homeLoader } from "@/pages/Home"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        loader: homeLoader,
+        action: homeAction,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
